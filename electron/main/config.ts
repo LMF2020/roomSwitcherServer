5101b56950cc5c3ad29d8b0219c5a6082df9c67b
Josh <jiangzx0526@gmail.com>
2024-05-20 00:42:49 +0800

    release 1.0.0

--- electron/main/config.ts
+++ electron/main/config.ts
@@ -3,7 +3,7 @@ import { fa, tr } from "element-plus/es/locales.mjs";
 export const config = {
   appName: "Rooms-切换器",
   version: "v1.0.0",
-  port: 3001,
+  port: 41794,
   exipreDate: "",
   width: 500,
   height: 550,
